eae8225affd1e99187734fc130ea7a85c4093c8c
David Moon <hrmoon99@gmail.com>
2023-03-17 03:19:19 -0500

    1.0.0

--- src/pages/clocks/analog.tsx
+++ src/pages/clocks/analog.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
+import styles from '../../styles/analog.module.sass'
 
 const Analog: React.FC = () => {
 
   return (
-    <main>
-      <Link href="/">Home</Link>
+    <main className={styles.content}>
+      <div className={styles.container}>
+        <div className={styles.box}></div>
+      </div>
     </main>
   );
 };
