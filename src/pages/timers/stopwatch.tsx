60b5de348ae2c0a600648e7b65d2482346aa07cd
David Moon <hrmoon99@gmail.com>
2023-03-27 03:26:49 -0500

    1.0.0

--- src/pages/timers/stopwatch.tsx
+++ src/pages/timers/stopwatch.tsx
@@ -9,12 +9,12 @@ function Stopwatch() {
       <Link href="/"><div className={styles.button}><ArrowLeft /></div></Link>
       <section className={styles.section}>
       <div className={`${styles.sec} ${styles.sec1}`}>
-          <h2 id="hour">00</h2>
-          <h2 id="dot">:</h2>
           <h2 id="minutes">00</h2>
+          <h2 id="dot">:</h2>
+          <h2 id="seconds">00</h2>
         </div>
         <div className={`${styles.sec} ${styles.sec2}`}>
-          <h2 id="seconds">00</h2>
+          <h2 id="miliseconds">00</h2>
           <div className={styles.container_button}>
             <button className={styles.start_button}>Start</button>
             <button className={styles.stop_button}>Stop</button>
