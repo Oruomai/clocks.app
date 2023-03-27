8a5a10172e1eab2dd5054fcc5d6fd3eeb3d12fc6
David Moon <hrmoon99@gmail.com>
2023-03-27 16:07:13 -0500

    1.0.0

--- src/pages/clocks/digital.tsx
+++ src/pages/clocks/digital.tsx
@@ -10,37 +10,35 @@ function Digital() {
       let minutes = document.getElementById('minutes');
       let seconds = document.getElementById('seconds');
       let ampm = document.getElementById('ampm');
-  
+
       fetch('https://worldtimeapi.org/api/ip')
         .then(response => response.json())
         .then(data => {
           let timezone = data.timezone;
           let now = new Date(data.datetime);
           let localTime = now.toLocaleTimeString('en-US', { timeZone: timezone });
-          let [h, m, s] = localTime.split(/:| /);
-          let am = parseInt(h) < 12 ? "AM" : "PM";
+          let [time, modifier] = localTime.split(' ');
+          let [h, m, s] = time.split(':');
           h = ((parseInt(h) % 12) || 12).toString();
-
-          h = (parseInt(h) < 10) ? "0" + h : h
-  
+          h = (parseInt(h) < 10) ? "0" + h : h;
+          ampm!.innerHTML = modifier;
           hours!.innerHTML = h;
           minutes!.innerHTML = m;
           seconds!.innerHTML = s;
-          ampm!.innerHTML = am;
         })
         .catch(error => console.error(error));
     }
-  
+
     var interval = setInterval(clock, 1000);
-  
+
     return () => clearInterval(interval);
   }, []);
 
   return (
     <main>
-    <Link href="/"><div className={styles.button}><ArrowLeft /></div></Link>
+      <Link href="/"><div className={styles.button}><ArrowLeft /></div></Link>
       <section className={styles.section}>
-      <div className={`${styles.sec} ${styles.sec1}`}>
+        <div className={`${styles.sec} ${styles.sec1}`}>
           <h2 id="hour">00</h2>
           <h2 id="dot">:</h2>
           <h2 id="minutes">00</h2>
